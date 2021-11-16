d6c7c0a47fc73bbca12e7c014f6408edf7972862
Aaron <afernandezr@cifpfbmoll.eu>
2021-11-16 19:40:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dialog/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.dialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void dialog(View v) {
+        setContentView(R.layout.activity_main);
+        new AlertDialog.Builder(this)
+                .setTitle("Dialeg de prova")
+                .setMessage("Això és un dialeg de prova")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with delete operation
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+
+}
